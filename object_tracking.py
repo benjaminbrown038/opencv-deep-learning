02e74fd6149de24b2469e2c3375c0f086366b3ee
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-08-04 17:06:17 -0400

    Update object_tracking.py

--- object_tracking.py
+++ object_tracking.py
@@ -4,3 +4,23 @@ import os
 import matplotlib.pyplot as plt 
 from moviepy.editor import VideoFileClip
 
+# Set up tracker.
+tracker_types = ['BOOSTING', 'CSRT', 'KCF', 'MEDIANFLOW', 'MIL', 'MOSSE', 'TLD']
+
+# Change the index to change the tracker type.
+tracker_type = tracker_types[0] 
+
+if tracker_type == 'BOOSTING':
+    tracker = cv2.legacy.TrackerBoosting_create()
+elif tracker_type == 'CSRT':
+    tracker = cv2.legacy.TrackerCSRT_create()
+elif tracker_type == 'KCF':
+    tracker = cv2.legacy.TrackerKCF_create()
+elif tracker_type == 'MEDIANFLOW':
+    tracker = cv2.legacy.TrackerMedianFlow_create()  
+elif tracker_type == 'MIL':
+    tracker = cv2.legacy.TrackerMIL_create()
+elif tracker_type == 'MOSSE':
+    tracker = cv2.legacy.TrackerMOSSE_create()
+elif tracker_type == 'TLD':
+    tracker = cv2.legacy.TrackerTLD_create()
